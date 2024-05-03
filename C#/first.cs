e1fa582212912b9addb2b7003e589882a854a950
vishal jaiswal <vishaljaiswal972000@gmail.com>
2024-05-03 16:43:03 +0530

    Add files via upload

---
+++ C#/first.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 5)
+            {
+                Console.WriteLine("Hello");
+                i++;
+            }
+        }
+    }
+}
